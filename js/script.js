52c6acfb4223c56755c1db4a5a276e7984202666
CompileError69 <s10247763@connect.np.edu.sg>
2023-02-14 03:20:32 +0800

    script.js update - 2 1. Added in codes to give the dropdown button a toggle ability 2. Added in codes for the arrow icon to toggle

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-function toggleBtn() {
+function dropdownToggle() {
     var content = document.getElementsByClassName("dropdown-content");
+    var button = document.getElementsByClassName("drop-btn")[0];
+    var arrowIcon = button.getElementsByClassName("arrow-icon")[0];
 
-    if (content.style.display === "none") {
-        content.style.display = "block";
-    }
-
-    else {
-        content.style.display = "none";
+    for (var i = 0; i < content.length; i++) {
+        if (content[i].style.display === "none") {
+            content[i].style.display = "block";
+            content[i].style.animation = "dropdownFadeIn 0.5s ease-in-out";
+            arrowIcon.innerHTML = '<i class="fas fa-caret-up"></i>';
+        } else {
+            content[i].style.display = "none";
+            content[i].style.animation = "dropdownFadeOut 0.5s ease-in-out";
+            arrowIcon.innerHTML = '<i class="fas fa-caret-down"></i>';
+        }
     }
 }
